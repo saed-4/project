7df3469db70b134b3f244bfd47849ec3bc17563b
Said Abdulhakim <saidabdulhakim2503@gmail.com>
2026-07-15 22:44:52 +0300

    updated exam file

--- exm.cpp
+++ exm.cpp
@@ -4,8 +4,7 @@ int main(){
 double temp[]={37.5,32.6,30,26,25,31,34};
 double minTemp = temp[0];
 double maxTemp = temp[0];
-double avg=0.0;
-double sum=0.0;
+
   int arraylength=sizeof(temp)/sizeof(temp[0]);
   for(int i =0; i<arraylength; i++){
     if(temp[i] < minTemp){ minTemp=temp[i]; }
